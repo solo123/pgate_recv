c2dfaafd8a3b1492b78613db3f6652d4a88e1abd
jimmy <solo123@21cn.com>
2016-09-26 03:39:15 +0800

    notify

--- app/controllers/recv_post_controller.rb
+++ app/controllers/recv_post_controller.rb
@@ -24,5 +24,41 @@ class RecvPostController < ApplicationController
     rv.detail = request.headers.to_h.ai(html: true)
     rv.status = 0
     rv.save
+
+    if rv.check_is_valid_notify
+      notify_client(rv.kaifu_result)
+    end
   end
+
+private
+  def notify_client(kaifu_result)
+    kaifu_result.init_validate
+    if kaifu_result.status < 7
+      kaifu_result.status += 1
+      kaifu_result.notify_time = Time.now
+      begin
+        uri = URI(kaifu_result.notify_url)
+        js = {
+          org_id: kaifu_result.organization_id,
+          order_id: kaifu_result.org_send_seq_id,
+          resp_code: kaifu_result.resp_code,
+          resp_desc: kaifu_result.resp_desc,
+          pay_result: kaifu_result.pay_result,
+          pay_desc: kaifu_result.pay_desc,
+          amount: kaifu_result.trans_amt,
+          fee: kaifu_result.fee
+        }
+        if kaifu_result.t0_resp_code == '00'
+          js[:pay_desc] += ' T0:' + kaifu_result.t0_resp_desc
+        end
+        resp = Net::HTTP.post_form(uri, data: js.to_json)
+        if resp.is_a?(Net::HTTPOK)
+          kaifu_result.status = 8
+        end
+      rescue => e
+      end
+      kaifu_result.save
+    end
+  end
+
 end
